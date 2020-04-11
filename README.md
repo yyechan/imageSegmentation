8858c802124d59c90843abe213e0ff6a9d5bd7de
yechan <yyechan@naver.com>
2020-04-11 15:35:45 +0900

    readme update

---
+++ README.md
@@ -0,0 +1,5 @@
+PRESSURE-SORES-SEGMENTATION
+
+2020-1학기 개별연구 딥러닝을 이용한 이미지내 형상 분석
+욕창이미지 Semantic Segmentation 및 형상분석
+
--- dataprocess.py
+++ dataprocess.py
@@ -1,3 +1,21 @@
 import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
 
-test = np.
+
+
+# Generates labels using most basic setup.  Supports various image sizes.  Returns image labels in same format
+# as original image.  Normalization matches MobileNetV2
+
+trained_image_width=512 
+mean_subtraction_value=127.5
+image = np.array(Image.open('./개별연구/data/14.jpg'))
+
+# resize to max dimension of images from training dataset
+
+resized_image = np.array(Image.fromarray(image.astype('uint8')).resize((512, 512)))
+
+img = Image.fromarray(resized_image, 'RGB')
+
+img.save('test.png')
+img.show()
